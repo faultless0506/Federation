19cfc2c5dad07b55ed675ccb0f9ca700830d542c
faultless0506 <azazellodaroncgsense@gmail.com>
2025-03-25 20:24:35 +0300

    detailed cards fixed

--- src/components/LessonsSchedule/LessonsSchedule.tsx
+++ src/components/LessonsSchedule/LessonsSchedule.tsx
@@ -14,36 +14,55 @@ const LessonsSchedule: React.FC = () => {
     return <div className="failed">Ошибка загрузки расписания</div>;
   }
 
+  // Группируем занятия по локациям
+  const lessonsByLocation = lessons.reduce((acc, lesson) => {
+    if (!acc[lesson.location]) {
+      acc[lesson.location] = [];
+    }
+    acc[lesson.location].push(lesson);
+    return acc;
+  }, {} as Record<string, typeof lessons>);
+
   return (
-    <section className="lessons-schedule" id="lessons">
+    <div className="lessons-schedule">
       <h3>Расписание занятий</h3>
-      <div className="lessons-schedule__table">
-        <div className="lessons-schedule__header">
-          <div className="lessons-schedule__cell">День недели</div>
-          <div className="lessons-schedule__cell">Время</div>
-          <div className="lessons-schedule__cell">Локация</div>
-          <div className="lessons-schedule__cell">Тренер</div>
-        </div>
-        <div className="lessons-schedule__body">
-          {lessons.map((lesson) => (
-            <div key={lesson.id} className="lessons-schedule__row">
-              <div className="lessons-schedule__cell" data-label="День недели">
-                {lesson.dayOfWeek}
+      {Object.entries(lessonsByLocation).map(([location, locationLessons]) => (
+        <div key={location} className="lessons-schedule__location">
+          <h4 className="lessons-schedule__location-title">{location}</h4>
+          <div className="lessons-schedule__table">
+            <div className="lessons-schedule__header">
+              <div className="lessons-schedule__cell">
+                <b>День недели</b>
               </div>
-              <div className="lessons-schedule__cell" data-label="Время">
-                {lesson.time}
+              <div className="lessons-schedule__cell">
+                <b>Время</b>
               </div>
-              <div className="lessons-schedule__cell" data-label="Локация">
-                {lesson.location}
-              </div>
-              <div className="lessons-schedule__cell" data-label="Тренер">
-                {lesson.trainer}
+              <div className="lessons-schedule__cell">
+                <b>Тренер</b>
               </div>
             </div>
-          ))}
+            <div className="lessons-schedule__body">
+              {locationLessons.map((lesson) => (
+                <div key={lesson.id} className="lessons-schedule__row">
+                  <div
+                    className="lessons-schedule__cell"
+                    data-label="День недели"
+                  >
+                    {lesson.dayOfWeek}
+                  </div>
+                  <div className="lessons-schedule__cell" data-label="Время">
+                    {lesson.time}
+                  </div>
+                  <div className="lessons-schedule__cell" data-label="Тренер">
+                    {lesson.trainer}
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
-      </div>
-    </section>
+      ))}
+    </div>
   );
 };
 
